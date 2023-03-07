8ed91a3c37257ab3f0999d65e6f3b1c92e4f5737
Giacomo Nanni <gn.nanni@gmail.com>
2023-03-08 00:31:53 +0100

    fix: check data before posting

--- src/routes/api/post/+server.js
+++ src/routes/api/post/+server.js
@@ -3,9 +3,16 @@ import db from "$lib/db"
 export const POST = async ({ request }) => {
     try {
         const data = await request.json();
-        const collection = await db.collection('paintdata');
-        collection.insertOne(data);
-        return new Response(JSON.stringify(data), { status: 200 })
+
+        console.log(data.dataPoints)
+        if (data.dataPoints && data.dataPoints[0][0] != null) {
+            const collection = await db.collection('paintdata');
+            collection.insertOne(data);
+            return new Response(JSON.stringify(data), { status: 200 })
+        }
+        else {
+            return new Response(JSON.stringify(data), { status: 422 })
+        }
 
     } catch (err) {
         console.log(err)
